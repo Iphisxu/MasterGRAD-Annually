22899cd6e5b7305260be39f8ba7e226eda982f2c
Evan <iphisxu@gmail.com>
2023-11-04 14:30:09 +0800

    绘制空间图，并有自定义绘图函数

---
+++ src/spatial.py
@@ -0,0 +1,188 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+
+from cnmaps import get_adm_maps, clip_quiver_by_map, clip_contours_by_map, draw_map
+import geopandas as gpd
+from shapely.ops import unary_union
+
+from matplotlib import rcParams
+config = {
+    "font.family":'Times New Roman',
+    "mathtext.fontset":'stix',
+    "font.serif": ['SimSun'],
+}
+rcParams.update(config)
+
+# silence the warning note
+import warnings
+warnings.filterwarnings("ignore")
+
+def plot_PRD_map(gridfile, cmin, cmax, cmstep, cbstep, 
+                 data_to_plot, uwind_to_plot, vwind_to_plot,
+                 ngrid=None, scale=None, headwidth=None, 
+                 title=None, colorbar_label=None,):
+    
+    '''
+    绘制珠三角地区的填色地图，包括污染物浓度和风向风速。
+
+    参数：
+    gridfile (array-like)：包含经度和纬度信息的网格文件。
+    cmin (float)：用于填色的污染物浓度的最小值。
+    cmax (float)：用于填色的污染物浓度的最大值。
+    cmstep (float)：等值线的步长。
+    cbstep (float)：色标带的步长。
+    data_to_plot (array-like)：要绘制的污染物浓度数据。
+    uwind_to_plot (array-like)：要绘制的东西向风速数据。
+    vwind_to_plot (array-like)：要绘制的南北向风速数据。
+    ngrid (int, optional)：风速箭头的密度。
+    scale (float, optional)：风速箭头的缩放比例。
+    headwidth (float, optional)：风速箭头的头部宽度。
+    title (str, optional)：地图的标题。
+    colorbar_label (str, optional)：颜色条的标签。
+    '''
+    
+    lon = gridfile.longitude
+    lat = gridfile.latitude
+    contourf_ticks = np.arange(cmin, cmax, cmstep)
+    colorbar_ticks = np.arange(cmin,cmax+1,cbstep)
+
+    fig = plt.figure(figsize=(12, 6), dpi=300)
+    ax = fig.subplots(1, 1, subplot_kw={'projection': ccrs.PlateCarree()})
+
+    # =============================================
+    # Defining map boundaries
+    # =============================================
+    
+    cities = ['广州市', '佛山市', '深圳市', '东莞市', '珠海市', '中山市', '惠州市', '江门市', '肇庆市']
+    multi_polygons = []
+
+    for city in cities:
+        polygon = get_adm_maps(city=city, record='first', only_polygon=True)
+        multi_polygons.append(polygon)
+        draw_map(polygon, color='gray', linewidth=0.8)
+
+    map_polygon = unary_union(multi_polygons)
+
+    gl = ax.gridlines(
+        xlocs=np.arange(-180, 180 + 1, 1), ylocs=np.arange(-90, 90 + 1, 1),
+        draw_labels=True, x_inline=False, y_inline=False,
+        linewidth=0, linestyle='--', color='gray')
+    gl.top_labels = False
+    gl.right_labels = False
+    gl.rotate_labels = False
+
+    ax.set_extent([111.2, 115.5, 21.4, 24.5], ccrs.PlateCarree())
+
+    # =============================================
+    # Pollutants concentration & Mask map
+    # =============================================
+
+    cf = ax.contourf(lon, lat, data_to_plot,
+                     cmap='Spectral_r',
+                     levels=contourf_ticks, extend='both',
+                     transform=ccrs.PlateCarree())
+
+    clip_contours_by_map(cf, map_polygon)
+
+    # =============================================
+    # Wind vector map & Mask map
+    # =============================================
+
+    xgrid = np.size(uwind_to_plot, 1)
+    ygrid = np.size(uwind_to_plot, 0)
+    if ngrid is not None and scale is not None and headwidth is not None:
+        qv = ax.quiver(lon[0:ygrid:ngrid, 0:xgrid:ngrid], lat[0:ygrid:ngrid, 0:xgrid:ngrid],
+                       uwind_to_plot[0:ygrid:ngrid, 0:xgrid:ngrid], vwind_to_plot[0:ygrid:ngrid, 0:xgrid:ngrid],
+                       transform=ccrs.PlateCarree(), color='k', alpha=1, scale=scale, headwidth=headwidth)
+        clip_quiver_by_map(qv, map_polygon)
+
+    # =============================================
+    # Defining title of the map and colorbar
+    # =============================================
+
+    if title is not None:
+        ax.set_title(title, loc='left', fontdict={'fontsize': 16, 'fontweight': 'bold'})
+
+    cbar = plt.colorbar(cf)
+    cbar.set_ticks(colorbar_ticks)
+    if colorbar_label is not None:
+        cbar.set_label(colorbar_label)
+
+    plt.show()
+
+def plot_PRD_diff(gridfile, cmin, cmax, cmstep, cbstep, 
+                  data1_to_plot, data2_to_plot,
+                  title=None, colorbar_label=None,):
+    
+    '''
+    绘制珠三角地区的污染物浓度差值填色图。
+
+    参数：
+    gridfile (array-like)：包含经度和纬度信息的网格文件。
+    cmin (float)：用于填色的污染物浓度的最小值。
+    cmax (float)：用于填色的污染物浓度的最大值。
+    cmstep (float)：等值线的步长。
+    cbstep (float)：色标带的步长。
+    data1_to_plot (array-like)：污染物浓度数据1。
+    data2_to_plot (array-like)：污染物浓度数据2。
+    title (str, optional)：地图的标题。
+    colorbar_label (str, optional)：颜色条的标签。
+    '''
+    
+    lon = gridfile.longitude
+    lat = gridfile.latitude
+    contourf_ticks = np.arange(cmin, cmax, cmstep)
+    colorbar_ticks = np.arange(cmin,cmax+1,cbstep)
+
+    fig = plt.figure(figsize=(12, 6), dpi=300)
+    ax = fig.subplots(1, 1, subplot_kw={'projection': ccrs.PlateCarree()})
+
+    # =============================================
+    # Defining map boundaries
+    # =============================================
+    
+    cities = ['广州市', '佛山市', '深圳市', '东莞市', '珠海市', '中山市', '惠州市', '江门市', '肇庆市']
+    multi_polygons = []
+
+    for city in cities:
+        polygon = get_adm_maps(city=city, record='first', only_polygon=True)
+        multi_polygons.append(polygon)
+        draw_map(polygon, color='gray', linewidth=0.8)
+
+    map_polygon = unary_union(multi_polygons)
+
+    gl = ax.gridlines(
+        xlocs=np.arange(-180, 180 + 1, 1), ylocs=np.arange(-90, 90 + 1, 1),
+        draw_labels=True, x_inline=False, y_inline=False,
+        linewidth=0, linestyle='--', color='gray')
+    gl.top_labels = False
+    gl.right_labels = False
+    gl.rotate_labels = False
+
+    ax.set_extent([111.2, 115.5, 21.4, 24.5], ccrs.PlateCarree())
+
+    # =============================================
+    # Pollutants concentration & Mask map
+    # =============================================
+
+    cf = ax.contourf(lon, lat, data2_to_plot - data1_to_plot,
+                     cmap='RdBu_r',
+                     levels=contourf_ticks, extend='both',
+                     transform=ccrs.PlateCarree())
+
+    clip_contours_by_map(cf, map_polygon)
+
+    # =============================================
+    # Defining title of the map and colorbar
+    # =============================================
+
+    if title is not None:
+        ax.set_title(title, loc='left', fontdict={'fontsize': 16, 'fontweight': 'bold'})
+
+    cbar = plt.colorbar(cf)
+    cbar.set_ticks(colorbar_ticks)
+    if colorbar_label is not None:
+        cbar.set_label(colorbar_label)
+
+    plt.show()
