5e687c9068621ff4a5d95a2c811817e4fea881b9
Evan <iphisxu@gmail.com>
2023-11-19 20:41:23 +0800

    在空间图中添加站点观测

--- src/spatial.py
+++ src/spatial.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+from matplotlib.colors import Normalize
 
 from cnmaps import get_adm_maps, clip_quiver_by_map, clip_contours_by_map, draw_map
 import geopandas as gpd
@@ -181,6 +182,172 @@ def plot_PRD_diff(gridfile, cmin, cmax, cmstep, cbstep,
 
     clip_contours_by_map(cf, map_polygon)
 
+    # =============================================
+    # Defining title of the map and colorbar
+    # =============================================
+
+    if title is not None:
+        ax.set_title(title, loc='left', fontdict={'fontsize': 16, 'fontweight': 'bold'})
+
+    cbar = plt.colorbar(cf)
+    cbar.set_ticks(colorbar_ticks)
+    if colorbar_label is not None:
+        cbar.set_label(colorbar_label)
+
+    plt.show()
+    
+
+def plot_map_withobs(gridfile, cmin, cmax, cmstep, cbstep, 
+                 data_to_plot, uwind_to_plot, vwind_to_plot,
+                 obsdata, obslon, obslat,
+                 ngrid=None, scale=None, headwidth=None, 
+                 mapcolor=None, title=None, colorbar_label=None,):
+
+    lon = gridfile.longitude
+    lat = gridfile.latitude
+    contourf_ticks = np.arange(cmin, cmax, cmstep)
+    colorbar_ticks = np.arange(cmin,cmax+0.01,cbstep)
+    if mapcolor is not None:
+        colormap = mapcolor
+    else:
+        colormap = 'Spectral_r'
+    
+    fig = plt.figure(figsize=(12, 6), dpi=300)
+    ax = fig.subplots(1, 1, subplot_kw={'projection': ccrs.PlateCarree()})
+
+    # =============================================
+    # Defining map boundaries
+    # =============================================
+    
+    cities = ['广州市', '佛山市', '深圳市', '东莞市', '珠海市', '中山市', '惠州市', '江门市', '肇庆市']
+    multi_polygons = []
+
+    for city in cities:
+        polygon = get_adm_maps(city=city, record='first', only_polygon=True)
+        multi_polygons.append(polygon)
+        draw_map(polygon, color='gray', linewidth=0.8)
+
+    map_polygon = unary_union(multi_polygons)
+
+    gl = ax.gridlines(
+        xlocs=np.arange(-180, 180 + 1, 1), ylocs=np.arange(-90, 90 + 1, 1),
+        draw_labels=True, x_inline=False, y_inline=False,
+        linewidth=0, linestyle='--', color='gray')
+    gl.top_labels = False
+    gl.right_labels = False
+    gl.rotate_labels = False
+
+    ax.set_extent([111.2, 115.5, 21.4, 24.5], ccrs.PlateCarree())
+
+    # =============================================
+    # Pollutants concentration & Mask map
+    # =============================================
+
+    cf = ax.contourf(lon, lat, data_to_plot,
+                     cmap=colormap,
+                     levels=contourf_ticks, extend='both',
+                     transform=ccrs.PlateCarree())
+
+    clip_contours_by_map(cf, map_polygon)
+
+    # =============================================
+    # Wind vector map & Mask map
+    # =============================================
+
+    xgrid = np.size(uwind_to_plot, 1)
+    ygrid = np.size(uwind_to_plot, 0)
+    if ngrid is not None and scale is not None and headwidth is not None:
+        qv = ax.quiver(lon[0:ygrid:ngrid, 0:xgrid:ngrid], lat[0:ygrid:ngrid, 0:xgrid:ngrid],
+                       uwind_to_plot[0:ygrid:ngrid, 0:xgrid:ngrid], vwind_to_plot[0:ygrid:ngrid, 0:xgrid:ngrid],
+                       transform=ccrs.PlateCarree(), color='k', alpha=1, scale=scale, headwidth=headwidth)
+        clip_quiver_by_map(qv, map_polygon)
+
+    # =============================================
+    # Observation stations scatter & Mask map
+    # =============================================
+    
+    norm = Normalize(vmin=cmin, vmax=cmax)
+    
+    cs = ax.scatter(obslon,obslat,c=obsdata,cmap=colormap,marker='o',s=20,
+                    edgecolors='k',linewidths=0.5,norm=norm,transform=ccrs.PlateCarree())
+    
+    # =============================================
+    # Defining title of the map and colorbar
+    # =============================================
+
+    if title is not None:
+        ax.set_title(title, loc='left', fontdict={'fontsize': 16, 'fontweight': 'bold'})
+
+    cbar = plt.colorbar(cf)
+    cbar.set_ticks(colorbar_ticks)
+    if colorbar_label is not None:
+        cbar.set_label(colorbar_label)
+
+    plt.show()
+    
+
+def plot_diff_withobs(gridfile, cmin, cmax, cmstep, cbstep, 
+                  data1_to_plot, data2_to_plot,
+                  obs1_to_plot, obs2_to_plot, 
+                  obslon, obslat,
+                  mapcolor=None, title=None, colorbar_label=None,):
+    
+    lon = gridfile.longitude
+    lat = gridfile.latitude
+    contourf_ticks = np.arange(cmin, cmax, cmstep)
+    colorbar_ticks = np.arange(cmin,cmax+0.01,cbstep)
+
+    fig = plt.figure(figsize=(12, 6), dpi=300)
+    ax = fig.subplots(1, 1, subplot_kw={'projection': ccrs.PlateCarree()})
+    if mapcolor is not None:
+        colormap = mapcolor
+    else:
+        colormap = 'RdBu_r'
+    
+    # =============================================
+    # Defining map boundaries
+    # =============================================
+    
+    cities = ['广州市', '佛山市', '深圳市', '东莞市', '珠海市', '中山市', '惠州市', '江门市', '肇庆市']
+    multi_polygons = []
+
+    for city in cities:
+        polygon = get_adm_maps(city=city, record='first', only_polygon=True)
+        multi_polygons.append(polygon)
+        draw_map(polygon, color='gray', linewidth=0.8)
+
+    map_polygon = unary_union(multi_polygons)
+
+    gl = ax.gridlines(
+        xlocs=np.arange(-180, 180 + 1, 1), ylocs=np.arange(-90, 90 + 1, 1),
+        draw_labels=True, x_inline=False, y_inline=False,
+        linewidth=0, linestyle='--', color='gray')
+    gl.top_labels = False
+    gl.right_labels = False
+    gl.rotate_labels = False
+
+    ax.set_extent([111.2, 115.5, 21.4, 24.5], ccrs.PlateCarree())
+
+    # =============================================
+    # Pollutants concentration & Mask map
+    # =============================================
+
+    cf = ax.contourf(lon, lat, data2_to_plot - data1_to_plot,
+                     cmap=colormap,
+                     levels=contourf_ticks, extend='both',
+                     transform=ccrs.PlateCarree())
+
+    clip_contours_by_map(cf, map_polygon)
+    
+    # =============================================
+    # Observation stations scatter & Mask map
+    # =============================================
+    
+    norm = Normalize(vmin=cmin, vmax=cmax)
+    
+    cs = ax.scatter(obslon,obslat,c=obs2_to_plot - obs1_to_plot,cmap=colormap,marker='o',s=20,
+                    edgecolors='k',linewidths=0.5,norm=norm,transform=ccrs.PlateCarree())
+    
     # =============================================
     # Defining title of the map and colorbar
     # =============================================
